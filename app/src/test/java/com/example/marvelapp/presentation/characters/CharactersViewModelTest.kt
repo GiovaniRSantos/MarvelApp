1ae12a969ba8087fb33b8ddbcbe61fbb979d434f
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-07-13 13:39:36 -0300

    test unitario CharactersViewModel

---
+++ app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.paging.PagingData
+import com.example.core.domain.model.Character
+import com.example.core.usecase.GetCharactersUseCase
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class CharactersViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    @Mock
+    lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    private lateinit var charactersViewModel: CharactersViewModel
+
+    private val pagindDataCharacters = PagingData.from(
+        listOf(
+            Character(
+                "A.I.M.",
+                "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+            )
+        )
+    )
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        charactersViewModel = CharactersViewModel(getCharactersUseCase)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should validate the paging data object values when calling charactersPagingData`() =
+        runBlockingTest {
+            whenever(getCharactersUseCase.invoke(any())).thenReturn(
+                flowOf(
+                    pagindDataCharacters
+                )
+            )
+
+            val result = charactersViewModel.charactersPagingData("")
+
+            assertEquals(1, result.count())
+        }
+
+    @ExperimentalCoroutinesApi
+    @Test(expected = RuntimeException::class)
+    fun `should throw an exception when the calling to the use case returns an exception`() =
+        runBlockingTest {
+            whenever(getCharactersUseCase.invoke(any()))
+                .thenThrow(RuntimeException())
+
+            charactersViewModel.charactersPagingData("")
+        }
+
+    @ExperimentalCoroutinesApi
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+}
