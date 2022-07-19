9598f25150853399962c9873752b9d064a928f30
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-07-18 21:23:25 -0300

    config R8

--- core/src/main/java/com/example/core/domain/model/Character.kt
+++ core/src/main/java/com/example/core/domain/model/Character.kt
@@ -1,7 +1,12 @@
 package com.example.core.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Character(
 
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("imageUrl")
     val imageUrl: String
 )
